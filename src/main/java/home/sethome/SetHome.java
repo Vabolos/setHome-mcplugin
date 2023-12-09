6eb63d62e892e3a519e53a6cfcdd11ecf6a975f2
Vabolos <vaboloslol@gmail.com>
2023-12-09 23:43:14 +0100

    command is now functional but still needs work

--- src/main/java/home/sethome/SetHome.java
+++ src/main/java/home/sethome/SetHome.java
@@ -1,44 +1,52 @@
 package home.sethome;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.logging.Level;
 
 public class SetHome extends JavaPlugin {
 
     private File dataFile;
+    private HashMap<String, HomeData> homesData;
+    private Gson gson;
 
     @Override
     public void onEnable() {
-        dataFile = new File(getDataFolder(), "..\\homes.json");
-        if (!dataFile.exists()) {
+        gson = new Gson();
+        dataFile = new File(getDataFolder(), "homes.json");
+        boolean fileExists = dataFile.exists();
+
+        if (!fileExists) {
             if (!dataFile.getParentFile().mkdirs()) {
                 getLogger().warning("Failed to create parent directories for homes.json.");
             }
 
             try {
-                boolean fileCreated = dataFile.createNewFile();
-                if (fileCreated) {
-                    getLogger().info("homes.json created successfully!");
-                } else {
-                    getLogger().warning("homes.json already exists!");
-                }
+                fileExists = dataFile.createNewFile();
             } catch (IOException e) {
                 getLogger().severe("Error creating homes.json file: " + e.getMessage());
             }
+        }
+
+        if (!fileExists) {
+            getLogger().warning("Failed to create homes.json.");
+        } else {
+            loadHomesData();
         }
 
         getLogger().info("SetHomePlugin has been enabled!");
@@ -46,11 +54,12 @@ public class SetHome extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        saveHomesData();
         getLogger().info("SetHomePlugin has been disabled!");
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("sethome") && sender instanceof Player) {
             Player player = (Player) sender;
             saveHomeLocation(player.getName(), player.getLocation());
@@ -73,35 +82,70 @@ public class SetHome extends JavaPlugin {
         return false;
     }
 
-
     private void saveHomeLocation(String playerName, Location location) {
-        FileConfiguration homesConfig = YamlConfiguration.loadConfiguration(dataFile);
-
-        homesConfig.set("homes." + playerName + ".x", location.getX());
-        homesConfig.set("homes." + playerName + ".y", location.getY());
-        homesConfig.set("homes." + playerName + ".z", location.getZ());
-        homesConfig.set("homes." + playerName + ".world", Objects.requireNonNull(location.getWorld()).getName());
-
-        try {
-            FileWriter writer = new FileWriter(dataFile);
-            homesConfig.save(String.valueOf(writer));
-            writer.close();
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Error saving home location for " + playerName, e);
+        if (homesData == null) {
+            homesData = new HashMap<>();
         }
+
+        homesData.put(playerName, new HomeData(location.getX(), location.getY(), location.getZ(), Objects.requireNonNull(location.getWorld()).getName()));
+        saveHomesData();
     }
 
     private Location getHomeLocation(String playerName) {
-        FileConfiguration homesConfig = YamlConfiguration.loadConfiguration(dataFile);
-
-        if (homesConfig.contains("homes." + playerName)) {
-            World world = getServer().getWorld(Objects.requireNonNull(homesConfig.getString("homes." + playerName + ".world")));
-            double x = homesConfig.getDouble("homes." + playerName + ".x");
-            double y = homesConfig.getDouble("homes." + playerName + ".y");
-            double z = homesConfig.getDouble("homes." + playerName + ".z");
-
-            return new Location(world, x, y, z);
+        if (homesData != null && homesData.containsKey(playerName)) {
+            HomeData homeData = homesData.get(playerName);
+            World world = getServer().getWorld(homeData.getWorld());
+            return new Location(world, homeData.getX(), homeData.getY(), homeData.getZ());
         }
         return null;
+    }
+
+    private void loadHomesData() {
+        try (FileReader reader = new FileReader(dataFile)) {
+            TypeToken<HashMap<String, HomeData>> token = new TypeToken<>() {};
+            homesData = gson.fromJson(reader, token.getType());
+
+            if (homesData == null) {
+                homesData = new HashMap<>();
+            }
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Error loading homes data: " + e.getMessage());
+        }
+    }
+
+    private void saveHomesData() {
+        try (FileWriter writer = new FileWriter(dataFile)) {
+            gson.toJson(homesData, writer);
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Error saving homes data: " + e.getMessage());
+        }
+    }
+
+    private static class HomeData {
+        private final double x, y, z;
+        private final String world;
+
+        public HomeData(double x, double y, double z, String world) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.world = world;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public double getZ() {
+            return z;
+        }
+
+        public String getWorld() {
+            return world;
+        }
     }
 }
